55540bed7143ee876b53120c63f95244787db373
Bradley Momberger <air_hadoken@yahoo.com>
2016-03-10 18:05:03 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Venn Diagram Generator
+
+This is a simple interactive page that allows you to create Venn diagrams in your browser and save them as PNG files.
+
+# License
+
+This project is copyright (C) 2016 Bradley Momberger and licensed under the MIT license. [https://opensource.org/licenses/MIT](https://opensource.org/licenses/MIT)
---
+++ venn.js
@@ -0,0 +1,241 @@
+
+var textareas = {
+  "1" : null,
+  "2" : null,
+  "3" : null,
+  "4" : null,
+  "12" : null,
+  "23" : null,
+  "34" : null,
+  "41" : null,
+  "123" : null,
+  "234" : null,
+  "341" : null,
+  "412" : null,
+  "1234" : null
+};
+
+
+function drawCircle(current, total) {
+
+  var svg = $("#output svg")[0];
+
+  var theta = current / total * Math.PI * 2;
+
+  // offsets.
+  var cx = 50 - Math.round(Math.cos(theta) * 50)/ 3;
+  var cy = 50 - Math.round(Math.sin(theta) * 50)/ 3;
+
+  // Expressed as percentage of total SVG width.
+  var radius = 390 / (+total + 12);
+
+  var fr = Math.floor(Math.random() * 255);
+  var fg = Math.floor(Math.random() * 255);
+  var fb = Math.floor(Math.random() * 255);
+
+  function zp(str) {
+    if(str.length < 2) {
+      return "0" + str;
+    } else {
+      return str;
+    }
+  } 
+
+  svg.innerHTML += [
+    '\n<ellipse cx="',
+    cx, '%" cy="',
+    cy, '%" rx="',
+    radius,
+    '%" ry="',
+    radius, 
+    '%" fill="rgba(',
+    fr.toString(10),
+    ", " ,
+    fg.toString(10),
+    ", " ,
+    fb.toString(10),
+    ', 0.5)" style="stroke:#',
+    zp(Math.floor((fr/2)).toString(16)),
+    zp(Math.floor((fg/2)).toString(16)),
+    zp(Math.floor((fb/2)).toString(16)), 
+    '; stroke-opacity: 0.5"></ellipse>'
+  ].join("");
+
+  return {
+    cx: cx,
+    cy: cy,
+    theta: theta
+  };
+}
+
+function drawCenteredText(key, x, y) {
+
+  var svg = $("#output svg")[0];
+
+  svg.innerHTML += "<text y='"
+                   + y
+                   + "' text-anchor='middle'></text>";
+  svg.lastChild.innerHTML = textareas[key] 
+                            ? textareaToTspans(textareas[key], svg.lastChild) 
+                            : "<tspan x='" + x + "'>[]</tspan>";
+  svg.lastChild.id = key + ":" + x ;
+}
+
+function textareaToTspans(textarea, textelement) {
+  var $textarea = $(textarea);
+  var textlines = $textarea.val().split("\n");
+  var fontsize = parseInt($(textelement).closest("svg").css("font-size"));
+
+  if(textlines.length === 0 || textlines.length === 1 && textlines[0] === "") {
+    textlines = ["[]"];
+  }
+
+  return textlines.map(function(snippet, i) {
+    return ["<tspan x='",
+            $(textelement).attr("id").split(":")[1],
+            "' dy='",
+            i === 0 ? fontsize / -2 * (textlines.length - 1) : fontsize,
+            "'>",
+            snippet,
+            "</tspan>"
+          ].join("");
+  }).join("");
+}
+
+$("#output").on("click", "svg text", function(ev) {
+  var textout = ev.currentTarget;
+  var $div;
+  var key = $(textout).attr("id").split(":")[0];
+  var $textarea = $(textareas[key] || "<textarea></textarea>");
+  textareas[key] = $textarea[0];
+
+  $("#output").append(
+    $div = $("<div>").css({"position":"absolute", left: $(this).offset().left, top: $(this).offset().top })
+            .append($textarea.text(
+              $(textout).children().map(function() {
+                return $(this).text();
+              }).get().join("\n")
+            ))
+            .append("<br>")
+            .append($("<button>Update</button>").on("click", function() {
+              $(textout).html(textareaToTspans($textarea, textout));
+              $textarea.detach(); // still referenced in textareas object
+              $div.remove();
+            }))
+  );
+  $textarea.focus();
+});
+
+function permuteCenters(fullcount, primitive_centers) {
+
+  var initial_fill = new Array(+fullcount).fill(0).map(function(_, i) {
+    return (i + 1);
+  });
+  var keyed_object = initial_fill.reduce(function(obj, i) {
+    var key = "", key_array;
+    for(var j = 0; j < fullcount - 1; j++) {
+      key = key + ((i + j - 1) % fullcount + 1).toString(10);
+      key_array = key.split("");
+
+      var theta = key_array.reduce(function(accumulator, idx, _i) {
+        if(_i > 0 && +idx < +key_array[0]) {
+          accumulator += Math.PI + Math.PI;
+        }
+        return accumulator + primitive_centers[idx].theta;
+      }, 0) / key.length;
+
+      var placement_r = key.split("").reduce(function(accumulator, idx) {
+        var item = primitive_centers[idx];
+        var offset_x = 50 - item.cx;
+        var offset_y = 50 - item.cy;
+        return accumulator + Math.sqrt(offset_x * offset_x + offset_y * offset_y);
+      }, 0) / key.length;
+
+      obj[key] = {
+        cx : 50 - placement_r * Math.cos(theta) / key.length * (+fullcount + 4)/4,
+        cy : 50 - placement_r * Math.sin(theta) / key.length * (+fullcount + 4)/4,
+        theta : theta
+      };
+    }
+    return obj;
+  }, {});
+  keyed_object[initial_fill.join("")] = {cx: 50, cy: 50, theta: 0};
+  return keyed_object;
+}
+
+
+$("#size_selector").on("change", function() {
+  var ncircs = $(this).val(),
+      _nc = ncircs;
+  var placement;
+  var numbers = [];
+  var centers = {};
+  $("#output svg").html("");
+  while(_nc--) {
+    centers[_nc + 1] = placement = drawCircle(_nc, ncircs);
+    numbers.unshift((_nc + 1).toString(10));
+  }
+  var permutedCenters = permuteCenters(ncircs, centers);
+  Object.keys(permutedCenters).forEach(function(key) {
+    var ctr = permutedCenters[key];
+    drawCenteredText(key, Math.round(ctr.cx) + "%", Math.round(ctr.cy) + "%");
+  });
+});
+
+$("#size_textbox").on("change", function() {
+  var val= $(this).val();
+  if(+val > 0) {
+    $("#output svg").attr("width", val).attr("height", val).css("font-size", function() {
+      return this.width.baseVal.value / 40;
+    });
+  }
+});
+
+
+$("#size_selector").trigger("change");
+
+
+$("#color_button").on("click", function() {
+  $("#output ellipse").attr("fill", function() {
+    return "rgba(" 
+      + Math.floor(Math.random() * 255) 
+      + ", " 
+      + Math.floor(Math.random() * 255) 
+      + ", " 
+      + Math.floor(Math.random() * 255) 
+      + ", 0.5)";
+  });
+});
+
+$("#export_button").on("click", function() {
+
+  var svg = $("#output svg");
+  var $canvas = $("<canvas>")
+                .attr("height", svg.attr("height"))
+                .attr("width", svg.attr("width"))
+                .css("display", "none")
+                .appendTo("#output");
+
+  var context = $canvas[0].getContext("2d");
+
+//  var imgsrc = 'data:image/svg+xml;base64,'+ btoa(svg.html());
+  var imgsrc = 'data:image/svg+xml;base64,'+ btoa(svg[0].outerHTML);
+  var image = new Image();
+  image.addEventListener("load", function() {
+    context.fillStyle = "white";
+    context.fillRect(0, 0, $canvas[0].width, $canvas[0].height);
+    context.drawImage(image, 0, 0);
+
+    var link = document.createElement('a');
+    link.download = svg.find("text:last").text() + ".png";
+    link.href = $canvas[0].toDataURL();
+    link.click();
+
+    $canvas.remove();
+  }, false);
+  image.addEventListener("error", function(ev, err) {
+    console.error(this);
+  });
+  image.src = imgsrc;
+});
+
